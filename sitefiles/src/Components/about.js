5277b07de6556677e1c6d3e177913680bfe6cc86
Miahruffin2 <contentcreator@jeremiahruffin.com>
2022-05-27 10:36:21 -0400

    added about content

--- sitefiles/src/Components/about.js
+++ sitefiles/src/Components/about.js
@@ -1,16 +1,23 @@
 import React from "react";
-import aboutimg from "./Images/banner.jpg"
-import { TiSocialLinkedin } from 'react-icons/ti'
-import { BsGithub } from 'react-icons/bs'
-import { BsInstagram } from 'react-icons/bs'
-import { FaTiktok } from 'react-icons/fa'
+import imagecard from "./Images/banner.jpg"
+
 
 export default function About() {
     return (
         <>
             <div className="about-fpage">
-                <h1 className="about-title">About Me</h1>
-                <p>An architech with an addiction to extremely spicy chicken! <br></br>Lover of technology, slow music, and Français. C'est moi, This is me!</p>
+                <h1 className="about-title">About Me </h1> 
+                <h2 className="Who">Who Am I?</h2>
+                <p className="about-paragraph">My name is <span className="sub-heading-detail">Jeremiah Ruffin</span>, and I am a Digital Marketing Specialist and Front-end Developer based in Detroit, MI. Currently studying CS at Wayne State University specializing in Web 3.0 Development!
+                </p>
+
+                <h2 className="Who">What do I do?</h2>
+                <p className="about-paragraph">I help companies research and develop marketing strategies to build their social media presence! Additionally, I produce front-end designs to maintain consistent brand identity!
+                </p>
+
+                <h2 className="Who">What Am I Passionate About?</h2>
+                <p className="about-paragraph"> I have a deep love for spicy food and Minecraft! I am also a Photograph, Je parle francais et J'adore slow music!
+                </p>
             </div>
         </>
     )
